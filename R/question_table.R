624caf5652faa3fe966cd6cb21b8ad881f964c2c
Stephen Childs <sechilds@gmail.com>
2019-06-12 10:20:35 -0400

    Add documentation to question_table.
    I added a couple of examples, including
    the example of adding an empty data frame.

--- R/question_table.R
+++ R/question_table.R
@@ -1,7 +1,20 @@
 #' Question Table
 #'
+#' Create a table that includes the full text of each survey question.
+#'
 #' Extract the text of each question from the labelled
 #' data loaded from the SPSS file.
+#'
+#' @param df The survey data frame. It should be labelled data.
+#' @param struct A data frame holding the structure of the survey.
+#'   This should include the field name and other information on
+#'   each question that gets merged into the question table.
+#' @return A tibble containing the field name, the full text of the
+#'   question and any survey structure information.
+#' @examples
+#' question_table(cgpss, data_frame(field_name=character()))
+#'
+#' question_table(nsse, nsse_structure)
 question_table <- function(df, struct) {
   questions <- df %>%
     var_label %>%
