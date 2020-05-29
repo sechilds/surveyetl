ab178a5a72f5bed0c1d6bffcd9142b77a5036fd9
Stephen Childs <sechilds@gmail.com>
2020-05-29 08:53:12 -0400

    Lower case field_name

--- R/compare_two_groups.R
+++ R/compare_two_groups.R
@@ -15,9 +15,9 @@ compare_two_groups <- function(df, comp_id, ...) {
   comp_id <- dplyr::enquo(comp_id)
   group_vars <- dplyr::enquos(...)
   df %>%
-    select(FIELD_NAME, SCORE, !!comp_id, !!!group_vars) %>%
+    select(field_name, score, !!comp_id, !!!group_vars) %>%
     mutate(value = !!comp_id) %>%
-    group_by(!!!group_vars, FIELD_NAME) %>%
+    group_by(!!!group_vars, field_name) %>%
     nest() %>%
     mutate(result = purrr::map(data, safe_stats)) %>%
     select(-data) %>%
